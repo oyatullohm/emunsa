b220c97d65f28e2b057359b52d02e42abcd193fe
oyatullohm <marziyahoni@gmail.com>
2024-05-07 20:17:02 +0500

    request login

--- main/decorator.py
+++
@@ -1,6 +0,0 @@
-def deco_login(fun):
-    def wrapper(request,pk=None,*args,**kwargs):
-        if request.user.is_authenticated  :
-            return fun(self, request, pk,*args, **kwargs)
-        return redirect('main:login')
-    return wrapper
--- main/views.py
+++ main/views.py
@@ -1,67 +1,71 @@
 from django.shortcuts import render ,redirect
 from django.views import View
 from .models import Product , Price
-from django.contrib.auth import authenticate, login ,logout
-from .decorator import deco_login
+from django.contrib.auth import authenticate, login ,logout 
 
 class HomeView(View):
-    # @deco_login
     def get(self,request):
-        product = Product.objects.all()
-        return render(request,'index.html',{'product':product})
-    # @deco_login
+        if request.user.is_authenticated :
+            product = Product.objects.all()
+            return render(request,'index.html',{'product':product})
+        return redirect('main:login')
     def post(self,request):
-        product = request.POST.get('product')
-        Product.objects.create(name=product)
-        return redirect ('main:home')
-
-# @deco_login
+        if request.user.is_authenticated :
+            product = request.POST.get('product')
+            Product.objects.create(name=product)
+            return redirect ('main:home')
+        return redirect('main:login')
 def detail(request,pk):
-    price =Price.objects.filter(product=pk)
-    return render(request,'detail.html',{'price':price,'product_id':pk})
+    if request.user.is_authenticated :
+        price =Price.objects.filter(product=pk)
+        return render(request,'detail.html',{'price':price,'product_id':pk})
+    return redirect('main:login')
 
 
-# @deco_login
 def price_create(request,pk):
-    product = Product.objects.get(id=int(pk))
-    color = request.POST.get('rang')
-    kl = request.POST.get('kl')
-    sum = request.POST.get('sum')
-    Price.objects.create(product=product,
-                         color=color
-                         ,kl=kl,
-                         sum=sum)
-    product_prise_all = Price.objects.filter(product=product)
-    summa = float()
-    for i in product_prise_all:
-        summa += i.sum
-    summa = summa / 30
-    product.sum = summa
-    product.save()
+    if request.user.is_authenticated :
+        product = Product.objects.get(id=int(pk))
+        color = request.POST.get('rang')
+        kl = request.POST.get('kl')
+        sum = request.POST.get('sum')
+        Price.objects.create(product=product,
+                            color=color
+                            ,kl=kl,
+                            sum=sum)
+        product_prise_all = Price.objects.filter(product=product)
+        summa = float()
+        for i in product_prise_all:
+            summa += i.sum
+        summa = summa / 30
+        product.sum = summa
+        product.save()
 
-    return redirect(f'/detail/{pk}')
+        return redirect(f'/detail/{pk}')
+    return redirect('main:login')
 
 
-# @deco_login
+
 def prise_update(request,pk):
-    price = Price.objects.get(id=int(pk))
-    color = request.POST.get('rang')
-    kl = request.POST.get('kl')
-    sum = request.POST.get('sum')
-    price.color = color
-    price.kl = kl
-    price.sum = sum
-    price.save()
+    if request.user.is_authenticated :
+        price = Price.objects.get(id=int(pk))
+        color = request.POST.get('rang')
+        kl = request.POST.get('kl')
+        sum = request.POST.get('sum')
+        price.color = color
+        price.kl = kl
+        price.sum = sum
+        price.save()
 
-    product= Product.objects.get( id = price.product.id)
-    product_prise_all = Price.objects.filter(product=product)
-    summa = float()
-    for i in product_prise_all:
-        summa += i.sum
-    summa = summa / 30
-    product.sum = summa
-    product.save()
-    return redirect(f'/detail/{product.id}')
+        product= Product.objects.get( id = price.product.id)
+        product_prise_all = Price.objects.filter(product=product)
+        summa = float()
+        for i in product_prise_all:
+            summa += i.sum
+        summa = summa / 30
+        product.sum = summa
+        product.save()
+        return redirect(f'/detail/{product.id}')
+    return redirect('main:login')
 
 def login_(request):
     if request.method == 'POST':
